1a2f0b3ea5a5c841ff80f185b2de7fa1e9f54a6d
shanizala <132726720+shanizala@users.noreply.github.com>
2023-09-14 08:17:44 +0530

    p-2-4.cpp

---
+++ p-2-4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	// function setw & endl
+	int m = 10;
+	cout<<"zalavadiya sani "<<endl;
+	cout<<"En_no    220130318007 "<<endl;
+
+	cout<<"value of a is : "<<m<<endl;// Function endl 
+	cout<<"This is for function setw : "<<setw(5)<<setfill('0')<<m; // function setw 
+	return 0;
+}

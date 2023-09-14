1b1d52c2a32e169bd7166eb5e0e8780c5e8c44d3
shanizala <132726720+shanizala@users.noreply.github.com>
2023-09-14 08:33:03 +0530

    p-2-16.cpp

---
+++ p-2-16.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class area
+{
+
+	private:
+		int l,b;
+		double c;
+		int a;
+	public:
+  
+		void Area(int l,int b)
+		{
+      cout<<"zalavadiya sani":
+      cout<<"220130318007";
+			cout<<"The Area Of Rectanle is ::"<<l*b<<endl;
+		}
+		void Area(double c)
+		{
+			cout<<"The Value Of A is ::"<<c<<endl;
+		}
+		void Area(int a)
+		{
+			cout<<"The Integer Value Of A Is ::"<<a<<endl;
+		}
+};
+int main()
+{
+	area a;
+	a.Area(2,3);
+	a.Area(4.5);
+	a.Area(20);
+	
+	return 0;
+}

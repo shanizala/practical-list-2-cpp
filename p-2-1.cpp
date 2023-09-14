d23fe8d53af5b3bf1c0fb8b23fa6fb57d78ef4f9
shanizala <132726720+shanizala@users.noreply.github.com>
2023-09-14 08:15:36 +0530

    p-2-1.cpp

---
+++ p-2-1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int m = 10;// Global variable 
+main()
+{
+	// Scope resolution program 
+	cout<<"I am zalavadiya sani"<<endl;
+	cout<<"En_no    220130318007 "<<endl;
+	
+	cout<<"value of Global variable is m & its value  ::  "<<::m;// scop resolution 
+	return 0;
+}

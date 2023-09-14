6536f0dd19c58db21ea8c5c0f8583bef2cde12ad
shanizala <132726720+shanizala@users.noreply.github.com>
2023-09-14 08:27:30 +0530

    p-2-9.cpp

---
+++ p-2-9.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+float  m,n;
+main()
+{
+	 
+	cout<<"zalavadiya sani "<<endl;
+	cout<<"En_no    220130318007 "<<endl;
+	cout<<"Enter the value of M ::  ";
+	cin>>::m;
+	cout<<"Enter the value of N ::  ";
+	cin>>::n;
+	
+	cout<<"value of M is   ::  "<<::m<<endl;
+	cout<<"Value of N is   ::  "<<::n<<endl;
+	cout<<"Sum of M & N    ::  "<<::m+::n<<setprecision(5);
+	return 0;
+}

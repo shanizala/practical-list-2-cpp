0af29367be344f025838f759a7083c61d296496a
shanizala <132726720+shanizala@users.noreply.github.com>
2023-09-14 08:18:38 +0530

    p-2-5.cpp

---
+++ p-2-5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	// function setw & endl
+	float m = 10.5555;
+	cout<<"zalavadiya sani"<<endl;
+	cout<<"En_no    220130318007 "<<endl;
+
+	//cout<<"The Value of A is ::"<<m;
+ 	cout<<"This is for function setprecision : "<<setprecision(8)<<m; // function setw 
+	return 0;
+}

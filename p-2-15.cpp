e6e494a4857093aefaaeca4a155eee9aad7ccb92
shanizala <132726720+shanizala@users.noreply.github.com>
2023-09-14 08:31:28 +0530

    p-2-15.cpp

---
+++ p-2-15.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+inline int Square(int a) 
+{
+	return a*a;
+}
+int main() 
+{
+	int a;
+	cout<<"zalavadiya sani"<<endl;
+	cout<<"220130318007"<<endl;
+	cout<<"Enter The Value ::"<<endl;
+	cin>>a;
+
+	cout<<"The Square Of The Number is ::"<<Square(a);
+
+	return 0;
+}

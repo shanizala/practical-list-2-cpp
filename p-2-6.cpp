bcf00fb2d2cdb04bedd1684887e3536c765ac1dd
shanizala <132726720+shanizala@users.noreply.github.com>
2023-09-14 08:19:37 +0530

    p-2-6.cpp

---
+++ p-2-6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+using namespace std; 
+int main()
+{
+	// function setfill 
+	int a =10;
+	cout<<"zalavadiya sani "<<endl;
+	cout<<"En_no    220130318007 "<<endl;
+
+	cout<<"The value of A :   "<<setw(5)<<setfill('*')<<a;
+	return 0;
+}

f7665c5d6b47ba767e52abb4bb19534dca260c51
shanizala <132726720+shanizala@users.noreply.github.com>
2023-09-14 08:13:37 +0530

    l2p2.cpp

---
+++ l2p2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int m = 10;// Global variable 
+main()
+{
+	// Scope resolution program 
+	cout<<"zalavadiya sani"<<endl;
+	cout<<"En_no    220130318007 "<<endl;
+	cout<<"value of Global variable is m & its value  ::  "<<::m;// scop resolution 
+	return 0;
+}
